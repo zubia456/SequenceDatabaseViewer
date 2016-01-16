c0670208130885bfb4cea3d5d2c44e58fab7546f
zubia456 <zubia861@gmail.com>
2016-01-15 19:47:22 -0500

    Update UNIPROTID.php

--- UNIPROTID.php
+++ UNIPROTID.php
@@ -2,19 +2,18 @@
 
 	header('Content-type: application/xml');
 	$link = mysql_connect('localhost', 'root', 'powers1');
-	mysql_select_db( 'project3_db', $link ) ;
+	mysql_select_db( 'mysql_db', $link ) ;
 	$sql = "SELECT uniproid FROM E2_sequence" ;
 	$result = mysql_query($sql) ;
 ?>
 <response>
 <?php
-		while($row = mysql_fetch_assoc($result))
-        	{
-
-        		$uniprotID = $row['uniproid'] ;
+	while($row = mysql_fetch_assoc($result))
+        {
+		$uniprotID = $row['uniproid'] ;
 ?>
-				<uniprotid><?php echo $uniprotID ?></uniprotid>
+		<uniprotid><?php echo $uniprotID ?></uniprotid>
 <?php
-}
+	}
 ?>
 </response>
